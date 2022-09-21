61431eaae6a295b63973a097f0659cf8988dc538
haengbeok <eodlf2932@gmail.com>
2022-09-21 18:39:55 +0900

    day-16-typeORM-mysql

---
+++ class/16-01-mysql-relation/src/apis/products/entities/product.entity.ts
@@ -0,0 +1,46 @@
+import { ProductCategory } from 'src/apis/productsCategories/entities/productCategory.entity';
+import { ProductSalesLocation } from 'src/apis/productsSalesLocations/entities/productSalesLocation.entity';
+import { ProductTag } from 'src/apis/productsTags/entities/productTag.entity';
+import { User } from 'src/apis/users/entities/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  isSoldout: boolean;
+
+  @JoinColumn()
+  @OneToOne(() => ProductSalesLocation)
+  productSalesLocation: ProductSalesLocation;
+
+  @ManyToOne(() => ProductCategory)
+  productCategory: ProductCategory;
+
+  @ManyToOne(() => User)
+  user: User;
+
+  @JoinTable()
+  @ManyToMany(() => ProductTag, (productTags) => productTags.products)
+  productTags: ProductTag[];
+}
