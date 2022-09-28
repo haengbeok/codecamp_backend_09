458df5971efeece39493d517e0d64d6d9e228df0
haengbeok <eodlf2932@gmail.com>
2022-09-28 19:22:38 +0900

    day-20-bcrypt-login

---
+++ class/20-01-signup-bcrypt/src/app.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { BoardModule } from './apis/boards/boards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { ProductsCategoriesModule } from './apis/productsCategories/productsCategories.module';
+import { ProductsModule } from './apis/products/products.module';
+import { UsersModule } from './apis/users/users.module';
+
+@Module({
+  imports: [
+    BoardModule,
+    ProductsCategoriesModule,
+    ProductsModule,
+    UsersModule,
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/commons/graphql/schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
+      entities: [__dirname + '/apis/**/*.entity.*'],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
+})
+export class AppModule {}
